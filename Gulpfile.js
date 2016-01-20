3a7ce47d18601edacc3f0f1528d2fa895c6ba2d1
Rodrigo Godoy <contato@rodrigogodoy.net>
2016-01-20 08:33:18 -0800

    Variaveis com os caminhos de pastas e gulp-watch [issue #1]

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,4 +3,12 @@ less = require ('gulp-less'),
 concat = require ('gulp-concat'),
 minifyCSS = require ('gulp-minify-css'),
 uglify = require ('gulp-uglify'),
-imagemin = require('gulp-imagemin');
+imagemin = require('gulp-imagemin'),
+watch = require ('gulp-watch');
+
+/*Caminhos dos arquivos e pastas, theme-name deve ser substituído pelo nome do tema*/
+var themeName = 'theme-name';
+var src = 'src/'+themeName;
+var dist = 'build/wp-content/'+themeName;
+
+gulp.task('default', ['css', 'js', 'watch']);
