d935d2c1d842222f02a8610e6d3bc3670b4bc72e
Rodrigo Godoy <contato@rodrigogodoy.net>
2016-01-20 08:25:50 -0800

    Carregado os plugins do gulp [issue #1]

---
+++ Gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require ('gulp'),
+less = require ('gulp-less'),
+concat = require ('gulp-concat'),
+minifyCSS = require ('gulp-minify-css'),
+uglify = require ('gulp-uglify'),
+imagemin = require('gulp-imagemin');
