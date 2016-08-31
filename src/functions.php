cc1cb002318d6b6bd7ac3c7fcf4ccce670c6b447
Rodrigo Godoy <contato@rodrigogodoy.net>
2016-08-31 08:50:04 -0300

    colocado css para o login do wordpress

--- src/functions.php
+++ src/functions.php
@@ -118,5 +118,10 @@ function my_post_gallery($output, $attr) {
 
     return $output;
 }
+/* Css para o login */
+function my_custom_login() {
+    echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/css/admin/login.css" />';
+}
+add_action('login_head', 'my_custom_login');
 
 ?>
